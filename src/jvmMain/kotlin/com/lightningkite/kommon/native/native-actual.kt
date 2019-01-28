1fd32ab52a63e09e7ad355a81193f69e847f1d28
Joseph Ivie <josephivie@gmail.com>
2019-01-28 01:19:02 -0700

    Partially done

--- src/jvmMain/kotlin/com/lightningkite/kommon/native/native-actual.kt
+++
@@ -1,7 +0,0 @@
-package com.lightningkite.kommon.native
-
-actual annotation class ThreadLocal()
-actual annotation class SharedImmutable()
-actual fun <T: Any> T.freeze(): T = this
-actual fun Any.ensureNeverFrozen() = Unit
-actual val Any.isFrozen: Boolean get() = false
