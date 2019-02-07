f456f403579a2b2c287cad23777f2ac90d1598bf
Joseph Ivie <josephivie@gmail.com>
2019-02-06 21:09:21 -0700

    Actual weakmap implementation

--- src/commonMain/kotlin/com/lightningkite/kommon/collection/WeakHashMap.kt
+++ src/commonMain/kotlin/com/lightningkite/kommon/collection/WeakHashMap.kt
@@ -1,3 +1,7 @@
 package com.lightningkite.kommon.collection
 
 expect class WeakHashMap<K: Any, V>() : MutableMap<K, V>
+
+fun WeakHashMap<*, *>.clean() {
+    this.size //Forces JVM and native to clean
+}
--- src/commonTest/kotlin/com/lightningkite/kommon/collection/SpecialMapsTest.kt
+++ src/commonTest/kotlin/com/lightningkite/kommon/collection/SpecialMapsTest.kt
@@ -4,15 +4,22 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class SpecialMapsTest{
+
+    data class AdvancedKey(val a: Int = 0)
+
     @Test
     fun isNormalMap(){
-        val keyA = "keyA"
-        val maps = listOf(ConcurrentHashMap<String, Int>(), WeakHashMap<String, Int>())
+        val keyA = AdvancedKey(0)
+        val keyB = AdvancedKey(1)
+        val maps = listOf(ConcurrentHashMap<AdvancedKey, Int>(), WeakHashMap<AdvancedKey, Int>())
         for(map in maps){
             map[keyA] = 0
-            assertTrue { map[keyA] == 0 }
+            assertTrue("a-0 for $map") { map[keyA] == 0 }
             map[keyA] = 1
-            assertTrue { map[keyA] == 1 }
+            assertTrue("a-1 for $map") { map[keyA] == 1 }
+            assertTrue("b-null for $map") { map[keyB] == null }
+            map[keyB] = 8
+            assertTrue("b-8 for $map") { map[keyB] == 8 }
         }
     }
 }
